7d577d9f0dcf1b7fdc9d906b0e1fe35c213cf198
Marshall Bowers <marshall@workos.com>
2022-04-18 16:59:48 -0400

    Use a stronger type for a `Profile`'s `connection_type`

--- src/sso/types/profile.rs
+++ src/sso/types/profile.rs
@@ -1,11 +1,15 @@
 use serde::{Deserialize, Serialize};
 
+use crate::KnownOrUnknown;
+
+use super::ConnectionType;
+
 /// [WorkOS Docs: Profile](https://workos.com/docs/reference/sso/profile)
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Profile {
-    pub id: String,
     pub object: String,
-    pub connection_type: String,
+    pub id: String,
+    pub connection_type: KnownOrUnknown<ConnectionType, String>,
     pub idp_id: String,
     pub email: String,
     pub first_name: String,
