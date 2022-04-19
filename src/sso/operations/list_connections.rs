b167a6159c7164c6b9db8ebb30accf7db7c5325b
Marshall Bowers <marshall@workos.com>
2022-04-19 11:29:44 -0400

    Add support for passing parameters to `list_connections`

--- src/sso/operations/list_connections.rs
+++ src/sso/operations/list_connections.rs
@@ -1,18 +1,26 @@
 use std::error::Error;
 
 use async_trait::async_trait;
+use serde::Serialize;
 
 use crate::sso::{Connection, ConnectionType, Sso};
 use crate::{KnownOrUnknown, PaginatedList};
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct ListConnectionsOptions<'a> {
+    #[serde(rename = "connection_type")]
     pub r#type: &'a Option<KnownOrUnknown<ConnectionType, String>>,
+    pub before: &'a Option<String>,
+    pub after: &'a Option<String>,
 }
 
 impl<'a> Default for ListConnectionsOptions<'a> {
     fn default() -> Self {
-        Self { r#type: &None }
+        Self {
+            r#type: &None,
+            before: &None,
+            after: &None,
+        }
     }
 }
 
@@ -38,6 +46,7 @@ impl<'a> ListConnections for Sso<'a> {
             .workos
             .client()
             .get(url)
+            .query(&options)
             .bearer_auth(self.workos.api_key())
             .send()
             .await?;
@@ -49,11 +58,11 @@ impl<'a> ListConnections for Sso<'a> {
 
 #[cfg(test)]
 mod test {
-    use crate::WorkOs;
+    use crate::{sso::ConnectionId, WorkOs};
 
     use super::*;
 
-    use mockito::{self, mock};
+    use mockito::{self, mock, Matcher};
     use serde_json::json;
     use tokio;
 
@@ -108,7 +117,63 @@ mod test {
 
         assert_eq!(
             paginated_list.metadata.before,
-            Some("prof_01DMC79VCBZ0NY2099737PSVF1".to_string())
+            Some("conn_01E2NPPCT7XQ2MVVYDHWGK1WN4".to_string())
+        )
+    }
+
+    #[tokio::test]
+    async fn it_calls_the_list_connections_endpoint_with_the_connection_type() {
+        let workos = WorkOs::builder(&"sk_example_123456789")
+            .base_url(&mockito::server_url())
+            .unwrap()
+            .build();
+
+        let _mock = mock("GET", "/connections")
+            .match_query(Matcher::UrlEncoded(
+                "connection_type".to_string(),
+                "OktaSAML".to_string(),
+            ))
+            .match_header("Authorization", "Bearer sk_example_123456789")
+            .with_status(200)
+            .with_body(
+                json!({
+                  "data": [
+                    {
+                      "object": "connection",
+                      "id": "conn_01E2NPPCT7XQ2MVVYDHWGK1WN4",
+                      "organization_id": "org_01EHWNCE74X7JSDV0X3SZ3KJNY",
+                      "connection_type": "OktaSAML",
+                      "name": "Example Co",
+                      "state": "active",
+                      "created_at": "2021-06-25T19:09:33.155Z",
+                      "updated_at": "2021-06-25T19:10:33.155Z"
+                    }
+                  ],
+                  "list_metadata": {
+                    "before": "conn_01E2NPPCT7XQ2MVVYDHWGK1WN4",
+                    "after": null
+                  }
+                })
+                .to_string(),
+            )
+            .create();
+
+        let paginated_list = workos
+            .sso()
+            .list_connections(&ListConnectionsOptions {
+                r#type: &Some(KnownOrUnknown::Known(ConnectionType::OktaSaml)),
+                ..Default::default()
+            })
+            .await
+            .unwrap();
+
+        assert_eq!(
+            paginated_list
+                .data
+                .into_iter()
+                .next()
+                .map(|connection| connection.id),
+            Some(ConnectionId::from("conn_01E2NPPCT7XQ2MVVYDHWGK1WN4"))
         )
     }
 }
