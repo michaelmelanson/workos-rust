d54b8fe1b171cbab42c8c7bab49614f24305f5e3
Marshall Bowers <marshall@workos.com>
2022-06-09 19:10:51 +0000

    Document `KnownOrUnknown` (#2)
    This PR documents the `KnownOrUnknown` type.

--- src/known_or_unknown.rs
+++ src/known_or_unknown.rs
@@ -1,8 +1,13 @@
 use serde::{Deserialize, Serialize};
 
+/// `KnownOrUnknown` is a type that respresents either a known value ([`Known`])
+/// or an unknown value ([`Unknown`]).
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum KnownOrUnknown<K, U> {
+    /// A known value.
     Known(K),
+
+    /// An unknown value.
     Unknown(U),
 }
