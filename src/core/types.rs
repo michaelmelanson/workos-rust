06cd8544e3ab829a5b150cd5092299c377581125
Marshall Bowers <marshall@workos.com>
2022-06-28 17:26:38 +0000

    Add support for serializing `Vec`s in query strings (#30)
    This PR adds support for serializing `Vec`s in query strings.
    
    To achieve this we introduce a new `UrlEncodableVec` tuple struct that wraps a `Vec<T>`. We then hand-implement `Serialize` in order to get the proper serialization.
    
    This type is only visible within the crate as its a bit of an implementation detail that we don't want SDK consumers to construct directly. Wherever we use this we'll want to add another wrapper struct that is visible outside of the crate.
    
    Resolves SDK-507.

--- src/core/types.rs
+++ src/core/types.rs
@@ -3,9 +3,11 @@ mod paginated_list;
 mod pagination_options;
 mod raw_attributes;
 mod timestamps;
+mod url_encodable_vec;
 
 pub use api_key::*;
 pub use paginated_list::*;
 pub use pagination_options::*;
 pub use raw_attributes::*;
 pub use timestamps::*;
+pub(crate) use url_encodable_vec::*;
---
+++ src/core/types/url_encodable_vec.rs
@@ -0,0 +1,113 @@
+use std::fmt::Display;
+use std::fmt::Write;
+
+use serde::Serialize;
+use serde::{ser, Serializer};
+
+/// A [`Vec`] that can be URL-encoded.
+#[derive(Debug)]
+pub(crate) struct UrlEncodableVec<T: Display>(Vec<T>);
+
+impl<T> Serialize for UrlEncodableVec<T>
+where
+    T: Display,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut serialized = String::new();
+
+        for (index, item) in self.0.iter().enumerate() {
+            write!(&mut serialized, "{}", item).map_err(|err| {
+                ser::Error::custom(format!("failed to write '{}': {}", item, err))
+            })?;
+
+            if index < self.0.len() - 1 {
+                write!(&mut serialized, ",").map_err(|err| {
+                    ser::Error::custom(format!("failed to write separator: {}", err))
+                })?
+            }
+        }
+
+        serializer.serialize_str(&serialized)
+    }
+}
+
+impl<T> From<Vec<T>> for UrlEncodableVec<T>
+where
+    T: Display,
+{
+    fn from(vec: Vec<T>) -> Self {
+        Self(vec)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use mockito::{self, mock, Matcher};
+    use reqwest::StatusCode;
+    use serde::Serialize;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn it_serializes_a_vec_in_the_query_string() {
+        #[derive(Debug, Serialize)]
+        struct List<'a> {
+            #[serde(rename = "items[]")]
+            pub items: UrlEncodableVec<&'a str>,
+        }
+
+        let _mock = mock("GET", "/")
+            .match_query(Matcher::UrlEncoded(
+                "items[]".to_string(),
+                "one,two,three".to_string(),
+            ))
+            .with_status(200)
+            .create();
+
+        let client = reqwest::Client::new();
+
+        let response = client
+            .get(&mockito::server_url())
+            .query(&List {
+                items: UrlEncodableVec(vec!["one", "two", "three"]),
+            })
+            .send()
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK)
+    }
+
+    #[tokio::test]
+    async fn it_serializes_a_vec_in_an_option_in_the_query_string() {
+        #[derive(Debug, Serialize)]
+        struct List<'a> {
+            #[serde(rename = "items[]")]
+            pub items: Option<UrlEncodableVec<&'a str>>,
+        }
+
+        let _mock = mock("GET", "/")
+            .match_query(Matcher::UrlEncoded(
+                "items[]".to_string(),
+                "one,two,three".to_string(),
+            ))
+            .with_status(200)
+            .create();
+
+        let client = reqwest::Client::new();
+
+        let response = client
+            .get(&mockito::server_url())
+            .query(&List {
+                items: Some(UrlEncodableVec(vec!["one", "two", "three"])),
+            })
+            .send()
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK)
+    }
+}
