5e2ae06d5053fff8ab85d0adc64ec675816218c6
Marshall Bowers <marshall@workos.com>
2022-06-28 15:08:55 +0000

    Document `PaginatedList` (#26)
    This PR adds documentation for `PaginatedList` and `ListMetadata`.
    
    Resolves SDK-506.

--- src/core/types/paginated_list.rs
+++ src/core/types/paginated_list.rs
@@ -1,15 +1,22 @@
 use serde::{Deserialize, Serialize};
 
+/// A paginated list of records.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PaginatedList<T> {
+    /// The list of items in the current page.
     pub data: Vec<T>,
 
+    /// The pagination metadata.
     #[serde(rename = "list_metadata")]
     pub metadata: ListMetadata,
 }
 
+/// The metadata for a [`PaginatedList`].
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ListMetadata {
+    /// The pagination cursor used to retrieve the previous page of records.
     pub before: Option<String>,
+
+    /// The pagination cursor used to retrieve the next page of records.
     pub after: Option<String>,
 }
