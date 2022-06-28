82345bbc8b84ff0ae637e414c4d23bae23bdc60d
Sheldon Vaughn <sheldonvaughn@icloud.com>
2022-06-28 20:25:27 +0000

    Add get_directory_user operation to directory_sync module (#36)
    Co-authored-by: Sheldon Vaughn <svaughn@sheldons-mbp.lan>

--- src/directory_sync/operations.rs
+++ src/directory_sync/operations.rs
@@ -1,7 +1,9 @@
 mod delete_directory;
 mod get_directory;
 mod get_directory_group;
+mod get_directory_user;
 
 pub use delete_directory::*;
 pub use get_directory::*;
 pub use get_directory_group::*;
+pub use get_directory_user::*;
---
+++ src/directory_sync/operations/get_directory_user.rs
@@ -0,0 +1,157 @@
+use async_trait::async_trait;
+use reqwest::StatusCode;
+use thiserror::Error;
+
+use crate::directory_sync::{DirectorySync, DirectoryUser, DirectoryUserId};
+use crate::{WorkOsError, WorkOsResult};
+
+/// An error returned from [`GetDirectoryUser`].
+#[derive(Debug, Error)]
+pub enum GetDirectoryUserError {}
+
+impl From<GetDirectoryUserError> for WorkOsError<GetDirectoryUserError> {
+    fn from(err: GetDirectoryUserError) -> Self {
+        Self::Operation(err)
+    }
+}
+
+/// [WorkOS Docs: Get a Directory User](https://workos.com/docs/reference/directory-sync/user/get)
+#[async_trait]
+pub trait GetDirectoryUser {
+    /// Retrieves a [`DirectoryUser`] by its ID.
+    ///
+    /// [WorkOS Docs: Get a Directory User](https://workos.com/docs/reference/directory-sync/user/get)
+    async fn get_directory_user(
+        &self,
+        id: &DirectoryUserId,
+    ) -> WorkOsResult<DirectoryUser, GetDirectoryUserError>;
+}
+
+#[async_trait]
+impl<'a> GetDirectoryUser for DirectorySync<'a> {
+    async fn get_directory_user(
+        &self,
+        id: &DirectoryUserId,
+    ) -> WorkOsResult<DirectoryUser, GetDirectoryUserError> {
+        let url = self
+            .workos
+            .base_url()
+            .join(&format!("/directory_users/{id}", id = id))?;
+        let response = self
+            .workos
+            .client()
+            .get(url)
+            .bearer_auth(self.workos.key())
+            .send()
+            .await?;
+
+        match response.error_for_status_ref() {
+            Ok(_) => {
+                let directory_user = response.json::<DirectoryUser>().await?;
+
+                Ok(directory_user)
+            }
+            Err(err) => match err.status() {
+                Some(StatusCode::UNAUTHORIZED) => Err(WorkOsError::Unauthorized),
+                _ => Err(WorkOsError::RequestError(err)),
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use matches::assert_matches;
+    use mockito::{self, mock};
+    use serde_json::json;
+    use tokio;
+
+    use crate::{ApiKey, WorkOs};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn it_calls_the_get_directory_user_endpoint() {
+        let workos = WorkOs::builder(&ApiKey::from("sk_example_123456789"))
+            .base_url(&mockito::server_url())
+            .unwrap()
+            .build();
+
+        let _mock = mock(
+            "GET",
+            "/directory_users/directory_user_01E1JG7J09H96KYP8HM9B0G5SJ",
+        )
+        .match_header("Authorization", "Bearer sk_example_123456789")
+        .with_status(200)
+        .with_body(
+            json!({
+              "id": "directory_user_01E1JG7J09H96KYP8HM9B0G5SJ",
+              "idp_id": "2836",
+              "directory_id": "directory_01ECAZ4NV9QMV47GW873HDCX74",
+              "emails": [{
+                "primary": true,
+                "type": "work",
+                "value": "marcelina@foo-corp.com"
+              }],
+              "first_name": "Marcelina",
+              "last_name": "Davis",
+              "username": "marcelina@foo-corp.com",
+              "groups": [{
+                "id": "",
+                "name": "Engineering",
+                "created_at": "2021-06-25T19:07:33.155Z",
+                "updated_at": "2021-06-25T19:07:33.155Z",
+                "raw_attributes": {"id": ""}
+              }],
+              "state": "active",
+              "created_at": "2021-06-25T19:07:33.155Z",
+              "updated_at": "2021-06-25T19:07:33.155Z",
+              "custom_attributes": {
+                "department": "Engineering"
+              },
+              "raw_attributes": {"department": "Engineering"}
+            })
+            .to_string(),
+        )
+        .create();
+
+        let directory_user = workos
+            .directory_sync()
+            .get_directory_user(&DirectoryUserId::from(
+                "directory_user_01E1JG7J09H96KYP8HM9B0G5SJ",
+            ))
+            .await
+            .unwrap();
+
+        assert_eq!(
+            directory_user.id,
+            DirectoryUserId::from("directory_user_01E1JG7J09H96KYP8HM9B0G5SJ")
+        )
+    }
+
+    #[tokio::test]
+    async fn it_returns_an_error_when_the_get_directory_user_endpoint_returns_unauthorized() {
+        let workos = WorkOs::builder(&ApiKey::from("sk_example_123456789"))
+            .base_url(&mockito::server_url())
+            .unwrap()
+            .build();
+
+        let _mock = mock("GET", "/directory_users/")
+            .match_header("Authorization", "Bearer sk_example_123456789")
+            .with_status(401)
+            .with_body(
+                json!({
+                    "message": "Unauthorized"
+                })
+                .to_string(),
+            )
+            .create();
+
+        let result = workos
+            .directory_sync()
+            .get_directory_user(&DirectoryUserId::from(""))
+            .await;
+
+        assert_matches!(result, Err(WorkOsError::Unauthorized))
+    }
+}
